7a473c7cf50cc7a3e8860b5d8ca4ed607edc2636
Julian Zanetti <julianzanetti99@gmail.com>
2025-08-07 15:14:44 -0300

    Modify infraestructure directory. Create terraform/ and cert-manager/

--- infrastructure/t7-elasticip.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_eip" "public_elastic_ip" {
-  depends_on = [ module.public_ec2_instance["1"], module.vpc ]
-  instance = module.public_ec2_instance["1"].id
-  domain = "vpc"
-  tags = merge(local.common_tags, {
-    Name = "homelab-public-elastic-ip"
-  })
-}
